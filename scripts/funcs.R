4465314a58dc6acbcd46e915307c726ffd9d35e1
Jonas Narkeliunas <jonas.narkeliunas@euromonitor.com>
2018-05-31 16:22:46 +0300

    Fixed smoothing, and added time column, renamed columns

--- scripts/funcs.R
+++ scripts/funcs.R
@@ -17,25 +17,32 @@ load_GPX <- function() {
     (function(x) x$trk) %>%
     (function(x) unlist(x[names(x) == "trkseg"], recursive = FALSE)) %>%
     map_df(function(x) as.data.frame(t(unlist(x)), stringsAsFactors=FALSE)) %>% 
-    select(-time) %>% 
+    mutate(time = lubridate::parse_date_time(time, orders = "ymdHMS")) %>%
+    mutate(time = time - min(time)) %>% 
     sapply(as.double) %>% 
     as.data.table()
     
-  colnames(gpx) <- c('ele', 'lat', 'lon')
+  colnames(gpx) <- c('ele', 'time', 'lat', 'lon')
   gpx
 }
 
 calculate_diffs <- function(gpx) {
-  gpx[, c('ele_next', 'lat_next', 'lon_next'):= 
-              list(lag(ele), lag(lat), lag(lon)),] %>% 
+  gpx[, ele:= smooth(ele, kind = '3RS3R'),] %>% 
+    .[, c('ele_next', 'lat_next', 'lon_next', 'time_next'):= 
+        list(lead(ele), lead(lat), lead(lon), lead(time)),] %>% 
     na.omit(.) %>%
-    .[, ele:= smooth(ele, kind = '3'),] %>% 
     .[, id:= .I,] %>% 
-    .[, gain:= as.numeric(ele_next - ele), by = id] %>% 
-    .[, displ:= geosphere::distGeo(c(lat, lon), c(lat_next, lon_next)),
+    .[, dT:= time_next - time, by = id] %>% 
+    .[, dH:= as.numeric(ele_next - ele), by = id] %>% 
+    .[, dS:= geosphere::distGeo(c(lat, lon), c(lat_next, lon_next)),
       by = id] %>% 
-    .[, dist:= round(cumsum(displ)*0.001,3),] %>% 
-    .[, grade:= round(gain/displ * 100, 1),] %>% 
-    select(-ele_next, -lat_next, -lon_next)
+    .[, dist:= round(cumsum(dS)*0.001,3),] %>% 
+    .[, grade:= round(dH/dS * 100, 1),] %>% 
+    select(id, time, dist, grade, dT, dS, dH)
 }
 
+# should give data table in the same schema as calculate_diffs returns
+# data should be of a race effort over trail terain throughout 
+get_gap_matrix <- function(data) {
+  
+}
